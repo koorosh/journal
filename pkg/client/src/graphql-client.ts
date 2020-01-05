351b0431f395640c4284b9bca4c6a3a1e16e337c
Andrii Vorobiov <and.vorobiov@gmail.com>
2020-01-05 16:29:22 +0200

    Fix env vars pointing to server url

--- pkg/client/src/graphql-client.ts
+++ pkg/client/src/graphql-client.ts
@@ -1,11 +1,9 @@
 import ApolloClient from 'apollo-boost'
 
-const serverPort = process.env.REACT_APP_SERVER_PORT || 8090
-const serverHost = process.env.REACT_APP_SERVER_HOST || window.location.hostname
-const serverProtocol = process.env.REACT_APP_SERVER_PROTOCOL || window.location.protocol
+const serverUrl = process.env.REACT_APP_SERVER_URL
 
 const client = new ApolloClient({
-  uri: `${serverProtocol}//${serverHost}:${serverPort}/graphql`,
+  uri: serverUrl,
 })
 
 export default client

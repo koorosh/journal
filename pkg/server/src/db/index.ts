fefa1968d2b6b1d595e41cb414f89aa93fd9bdda
Andrii Vorobiov <and.vorobiov@gmail.com>
2020-04-04 12:52:40 +0300

    server: (fix) Database connection string to MongoDB Atlas

--- pkg/server/src/db/index.ts
+++ pkg/server/src/db/index.ts
@@ -4,13 +4,16 @@ const host = process.env.MONGODB_HOST
 const user = process.env.MONGODB_USER
 const password = process.env.MONGODB_PASSWORD
 const uriFormat = process.env.MONGODB_URI_FORMAT
+const authSource = process.env.MONGODB_AUTH_SOURCE
+const isProduction = process.env.NODE_ENV === 'production'
 export const SYSTEM_TENANT_ID = process.env.MONGODB_DATABASE
 
 const dbOptions: ConnectionOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
-  authSource: SYSTEM_TENANT_ID
+  // it was necessary to manage connection to MongoDb Atlas cluster
+  authSource,
 }
 
 const connectionsCache = new Map<string, mongoose.Connection>()
@@ -19,7 +22,10 @@ export const getConnectionByTenantId = (tenantId: string = SYSTEM_TENANT_ID) => 
   if (connectionsCache.has(tenantId)) {
     return connectionsCache.get(tenantId)
   }
-  const connection = mongoose.createConnection(`${uriFormat}://${user}:${password}@${host}/${tenantId}`, dbOptions)
+  const connection = mongoose.createConnection(
+    `${uriFormat}://${user}:${password}@${host}/${tenantId}${isProduction ? '?retryWrites=true&w=majority' : ''}`,
+    dbOptions
+  )
   connectionsCache.set(tenantId, connection)
   return connection
 }
